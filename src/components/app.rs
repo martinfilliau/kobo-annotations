f070b1490368581042d0b1294d72028f7f194dcf
Martin Filliau <martin@filliau.com>
2025-01-20 21:22:18 +0100

    poc Upload .epub, read title

--- src/components/app.rs
+++ src/components/app.rs
@@ -1,32 +1,9 @@
+use crate::components::file_upload::FileUpload;
 use leptos::prelude::*;
 
 #[component]
 pub fn App() -> impl IntoView {
-    let (count, set_count) = signal(0);
-
     view! {
-        <button
-            on:click=move |_| {
-                *set_count.write() += 1;
-            }
-        >"Increment"
-        </button>
-        <button
-            on:click=move |_| {
-                *set_count.write() -= 1;
-            }
-        >"Decrement"
-        </button>
-        <p
-            class:red=move || is_odd(count.get())
-        >{count}</p>
-        <p>
-            "Double count: "
-            {move || count.get() * 2}
-        </p>
+        <FileUpload />
     }
-}
-
-fn is_odd(count: usize) -> bool {
-    count % 2 == 1
 }
